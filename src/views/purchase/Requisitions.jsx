ec6b2780164a64bced436fa54d248f522872de9a
KelzangUD <kuding755@gmail.com>
2024-11-27 12:51:19 +0600

    requisition ui update

--- src/views/purchase/Requisitions.jsx
+++ src/views/purchase/Requisitions.jsx
@@ -3,6 +3,7 @@ import {
   Autocomplete,
   Box,
   Button,
+  Card,
   Paper,
   Grid,
   TextField,
@@ -167,107 +168,14 @@ const Requisitions = () => {
   return (
     <>
       <Box sx={{ px: 2 }}>
-        <Grid container spacing={4} alignItems="center">
+        <Grid container alignItems="center">
           <Grid item xs={12}>
-            <Paper elevation={1}>
-              <Grid container padding={2}>
-                <Grid container spacing={2}>
-                  <Grid item xs={3}>
-                    <TextField
-                      label="Requisiton Number"
-                      variant="outlined"
-                      fullWidth
-                      name="requisition_number"
-                      disabled
-                      required
-                      size="small"
-                    />
-                  </Grid>
-                  <Grid item xs={3}>
-                    <FormControl fullWidth size="small">
-                      <InputLabel id="requisition-type-select-label">
-                        Requisition Type*
-                      </InputLabel>
-                      <Select
-                        labelId="requisition-type-select-label"
-                        id="requisition-type-select"
-                        value={requisitionData?.requisitionType}
-                        label="Requisition Type*"
-                        required
-                        onChange={requisitionTypeHandle}
-                      >
-                        {requisitionType?.map((item) => (
-                          <MenuItem value={item?.id} key={item?.id}>
-                            {item?.type}
-                          </MenuItem>
-                        ))}
-                      </Select>
-                    </FormControl>
-                  </Grid>
-                  <Grid item xs={3}>
-                    <FormControl fullWidth>
-                      <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DatePicker
-                          label="Requisition Date*"
-                          value={dayjs(requisitionData?.requisitionDate)}
-                          onChange={requisitionDateHandle}
-                          disabled
-                          slotProps={{
-                            textField: {
-                              size: "small",
-                            },
-                          }}
-                        />
-                      </LocalizationProvider>
-                    </FormControl>
-                  </Grid>
-                  <Grid item xs={3} display="flex">
-                    <FormControl fullWidth>
-                      <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DatePicker
-                          label="Need By Date*"
-                          value={dayjs(requisitionData?.needByDate)}
-                          onChange={needByDateHandle}
-                          slotProps={{
-                            textField: {
-                              size: "small",
-                            },
-                          }}
-                        />
-                      </LocalizationProvider>
-                    </FormControl>
-                  </Grid>
-                </Grid>
-                <Grid container spacing={2} sx={{ mt: 1 }}>
-                  <Grid item xs={3}>
-                    <TextField
-                      label="Employee Name"
-                      variant="outlined"
-                      fullWidth
-                      name="employee_name"
-                      value={userDetails?.userName}
-                      required
-                      disabled
-                      size="small"
-                    />
-                  </Grid>
-                  <Grid item xs={3} display="flex">
-                    <TextField
-                      label="Region"
-                      variant="outlined"
-                      fullWidth
-                      name="region"
-                      disabled
-                      value={userDetails?.region}
-                      required
-                      size="small"
-                    />
-                  </Grid>
-                </Grid>
-                <Grid item xs={12} marginTop={4}>
+            <>
+              <Grid container>
+                <Card sx={{ width: "100%" }}>
                   <Grid
                     container
-                    padding={1}
+                    p={2}
                     sx={{
                       display: "flex",
                       justifyContent: "space-between",
@@ -277,75 +185,195 @@ const Requisitions = () => {
                     }}
                   >
                     <Grid item>
-                      <Typography variant="subtitle1">Item Details</Typography>
+                      <Typography variant="subtitle1">
+                        Requsition Details
+                      </Typography>
                     </Grid>
                   </Grid>
-                  <Grid container paddingY={2}>
-                    <Grid container spacing={2}>
-                      <Grid item xs={3}>
-                        <Autocomplete
-                          disablePortal
-                          options={requisitionItems?.map((item) => ({
-                            label: item?.itemDescription,
-                            value: item?.id,
-                            item_Number: item?.itemNumber,
-                            uom: item?.uom,
-                          }))}
-                          onChange={selectItemHandle}
-                          value={itemDTOList?.item_Description}
-                          renderInput={(params) => (
-                            <TextField {...params} label="Description" size="small" />
-                          )}
-                        />
+                  <Grid container spacing={1} my={1} px={2}>
+                    <Grid item xs={3}>
+                      <TextField
+                        label="Requisiton Number"
+                        variant="outlined"
+                        fullWidth
+                        name="requisition_number"
+                        disabled
+                        required
+                        size="small"
+                      />
+                    </Grid>
+                    <Grid item xs={3}>
+                      <FormControl fullWidth size="small">
+                        <InputLabel id="requisition-type-select-label">
+                          Requisition Type*
+                        </InputLabel>
+                        <Select
+                          labelId="requisition-type-select-label"
+                          id="requisition-type-select"
+                          value={requisitionData?.requisitionType}
+                          label="Requisition Type*"
+                          required
+                          onChange={requisitionTypeHandle}
+                        >
+                          {requisitionType?.map((item) => (
+                            <MenuItem value={item?.id} key={item?.id}>
+                              {item?.type}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={3}>
+                      <FormControl fullWidth>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                          <DatePicker
+                            label="Requisition Date*"
+                            value={dayjs(requisitionData?.requisitionDate)}
+                            onChange={requisitionDateHandle}
+                            disabled
+                            slotProps={{
+                              textField: {
+                                size: "small",
+                              },
+                            }}
+                          />
+                        </LocalizationProvider>
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={3} display="flex">
+                      <FormControl fullWidth>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                          <DatePicker
+                            label="Need By Date*"
+                            value={dayjs(requisitionData?.needByDate)}
+                            onChange={needByDateHandle}
+                            slotProps={{
+                              textField: {
+                                size: "small",
+                              },
+                            }}
+                          />
+                        </LocalizationProvider>
+                      </FormControl>
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={1} mt={1} mb={2} px={2}>
+                    <Grid item xs={3}>
+                      <TextField
+                        label="Employee Name"
+                        variant="outlined"
+                        fullWidth
+                        name="employee_name"
+                        value={userDetails?.userName}
+                        required
+                        disabled
+                        size="small"
+                      />
+                    </Grid>
+                    <Grid item xs={3} display="flex">
+                      <TextField
+                        label="Region"
+                        variant="outlined"
+                        fullWidth
+                        name="region"
+                        disabled
+                        value={userDetails?.region}
+                        required
+                        size="small"
+                      />
+                    </Grid>
+                  </Grid>
+                </Card>
+                <Grid item xs={12} marginTop={2}>
+                  <Card>
+                    <Grid
+                      container
+                      padding={2}
+                      sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        backgroundColor: "#1976d2",
+                        color: "#eee",
+                      }}
+                    >
+                      <Grid item>
+                        <Typography variant="subtitle1">
+                          Item Details
+                        </Typography>
                       </Grid>
-                      <Grid item xs={3}>
-                        <TextField
-                          label="Item Number"
-                          variant="outlined"
-                          fullWidth
-                          name="item_number"
-                          disabled
-                          value={itemDTOList?.item_Number}
-                          size="small" 
-                        />
-                      </Grid>
-                      <Grid item xs={3}>
-                        <TextField
-                          label="UOM"
-                          variant="outlined"
-                          fullWidth
-                          name="uom"
-                          disabled
-                          value={itemDTOList?.uom}
-                          size="small" 
-                        />
-                      </Grid>
-                      <Grid item xs={3} display="flex">
-                        <Grid>
-                          <TextField
-                            label={isETop ? "Amount" : "Required Quantity"}
-                            variant="outlined"
-                            fullWidth
-                            name="amount"
-                            type="number"
-                            onChange={itemDTOListAmountHandle}
-                            value={itemDTOList?.amount}
-                            size="small" 
+                    </Grid>
+                    <Grid container padding={2}>
+                      <Grid container spacing={1}>
+                        <Grid item xs={3}>
+                          <Autocomplete
+                            disablePortal
+                            options={requisitionItems?.map((item) => ({
+                              label: item?.itemDescription,
+                              value: item?.id,
+                              item_Number: item?.itemNumber,
+                              uom: item?.uom,
+                            }))}
+                            onChange={selectItemHandle}
+                            value={itemDTOList?.item_Description}
+                            renderInput={(params) => (
+                              <TextField
+                                {...params}
+                                label="Description"
+                                size="small"
+                              />
+                            )}
                           />
                         </Grid>
-                        <Grid alignContent="center">
-                          <IconButton
-                            aria-label="add"
-                            onClick={addItemListButtonHandle}
-                          >
-                            <AddBoxIcon />
-                          </IconButton>
+                        <Grid item xs={3}>
+                          <TextField
+                            label="Item Number"
+                            variant="outlined"
+                            fullWidth
+                            name="item_number"
+                            disabled
+                            value={itemDTOList?.item_Number}
+                            size="small"
+                          />
+                        </Grid>
+                        <Grid item xs={3}>
+                          <TextField
+                            label="UOM"
+                            variant="outlined"
+                            fullWidth
+                            name="uom"
+                            disabled
+                            value={itemDTOList?.uom}
+                            size="small"
+                          />
+                        </Grid>
+                        <Grid item xs={3} display="flex">
+                          <Grid>
+                            <TextField
+                              label={isETop ? "Amount" : "Required Quantity"}
+                              variant="outlined"
+                              fullWidth
+                              name="amount"
+                              type="number"
+                              onChange={itemDTOListAmountHandle}
+                              value={itemDTOList?.amount}
+                              size="small"
+                            />
+                          </Grid>
+                          <Grid alignContent="center">
+                            <IconButton
+                              aria-label="add"
+                              onClick={addItemListButtonHandle}
+                            >
+                              <AddBoxIcon />
+                            </IconButton>
+                          </Grid>
                         </Grid>
                       </Grid>
                     </Grid>
-                  </Grid>
+                  </Card>
                 </Grid>
-                <Grid container spacing={2} sx={{ mt: 1 }} paddingLeft={2}>
+                <Grid container spacing={2} sx={{ mt: 2 }} paddingLeft={2}>
                   <TableContainer component={Paper}>
                     <Table
                       sx={{ minWidth: 650 }}
@@ -372,9 +400,7 @@ const Requisitions = () => {
                               </TableCell>
                               <TableCell>{item?.item_Number}</TableCell>
                               <TableCell>{item?.uom}</TableCell>
-                              <TableCell>
-                                {item?.amount}
-                              </TableCell>
+                              <TableCell>{item?.amount}</TableCell>
                               <TableCell>
                                 <IconButton
                                   aria-label="remove"
@@ -391,12 +417,12 @@ const Requisitions = () => {
                   </TableContainer>
                 </Grid>
               </Grid>
-            </Paper>
+            </>
           </Grid>
-          <Grid container display="flex" justifyContent="flex-end" marginY={4}>
+          <Grid container display="flex" justifyContent="flex-end" marginY={2}>
             <Button
               variant="contained"
-              sx={{ marginRight: 2 }}
+              sx={{ marginRight: 1 }}
               onClick={createHandle}
               size="small"
             >
