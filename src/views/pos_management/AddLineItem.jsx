b13da26feab232ba1259d99cb74b201735488530
KelzangUD <kuding755@gmail.com>
2026-04-01 14:35:36 +0600

    amount u

--- src/views/pos_management/AddLineItem.jsx
+++ src/views/pos_management/AddLineItem.jsx
@@ -1028,7 +1028,7 @@ const AddLineItem = ({
                         Number(lineItemDetail?.actualDownPayment) +
                         Number(lineItemDetail?.payableAmount) +
                         Number(lineItemDetail?.serviceCharge) +
-                        Number(lineItemDetail?.taxAmt)
+                        Number(parseFloat(lineItemDetail?.taxAmt).toFixed(2))
                       }
                     />
                   </Grid>
